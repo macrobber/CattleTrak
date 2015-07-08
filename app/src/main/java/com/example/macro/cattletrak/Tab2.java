485a8fffee11c944505c028d498f420d0012c496
Ron <macrobber@gmail.com>
2015-07-08 10:29:42 -0400

    Revert

---
+++ app/src/main/java/com/example/macro/cattletrak/Tab2.java
@@ -0,0 +1,22 @@
+package com.example.macro.cattletrak;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hp1 on 21-01-2015.
+ */
+public class Tab2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.tab_2,container,false);
+        return v;
+    }
+}
